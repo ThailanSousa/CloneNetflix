2dec14531cb305fa54548267fd855a520acb2b4b
ThailanSousa <joaot.sousa@unifacol.edu.br>
2023-09-19 14:43:31 -0300

    atualização do enum

--- NetflixClone/src/main/java/br/com/unifacol/cloneflix/enums/Message.java
+++ NetflixClone/src/main/java/br/com/unifacol/cloneflix/enums/Message.java
@@ -4,7 +4,13 @@ public enum Message {
   MODELOCPF("O padrão do CPF é: XXXXXXXXXXX"),
   INVALID_EMAIL("E-mail inválido."),
   CAMPO_OBRIGATORIO("Este campo é obrigatório"),
-  SUCESSO("Cadastro Realizado com Sucesso!!");
+  SUCESSO("Cadastro Realizado com Sucesso!!"),
+  NAME("Digite o novo nome: "),
+  AGE("Digite a nova idade: "),
+  CPF("Digite seu CPF: "),
+  PHONE("Digite seu telefone: "),
+  EMAIL("Digite seu E-mail: "),
+  PASSWORD("Digite sua senha: ");
 
 
 
