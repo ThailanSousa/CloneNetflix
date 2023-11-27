8aa0dae2b29807034cb95d92c390e44c45fe7067
ThailanSousa <joaot.sousa@unifacol.edu.br>
2023-11-27 18:46:09 -0300

    repositoroi de funcionario padronizado

--- cloneflix/src/main/java/com/cloneflix/repository/FuncionarioRepository.java
+++ cloneflix/src/main/java/com/cloneflix/repository/FuncionarioRepository.java
@@ -3,6 +3,9 @@ package com.cloneflix.repository;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Root;
 
 import com.cloneflix.model.entities.Funcionario;
 
@@ -13,7 +16,7 @@ public class FuncionarioRepository {
     public FuncionarioRepository() {
         this.emf = Persistence.createEntityManagerFactory("cloneflix");
     }
-
+    //metodo de cadastro funcionario baseado no cliente !!!
     public boolean cadastrarFuncionario(Funcionario funcionario) {
         try {
             EntityManager em = emf.createEntityManager();
@@ -27,13 +30,27 @@ public class FuncionarioRepository {
             return false;
         }
     }
-
-    public Funcionario consultarFuncionario(Long id) {
+    
+    
+    //metodo de consutar funcionario baseado no cliente !!!
+    public Funcionario consultarFuncioanrioPorCpf(String cpf) {
         EntityManager em = emf.createEntityManager();
-        return em.find(Funcionario.class, id);
+        try {
+            CriteriaBuilder cb = em.getCriteriaBuilder();
+            CriteriaQuery<Funcionario> cq = cb.createQuery(Funcionario.class);
+            Root<Funcionario> root = cq.from(Funcionario.class);
+            cq.select(root).where(cb.equal(root.get("cpf"), cpf));
+            return em.createQuery(cq).getSingleResult();
+        } catch (Exception e) {
+            System.out.println("Funcionario não encontrado.");
+            return null;
+        } finally {
+            em.close();
+        }
     }
-
-    public boolean atualizarFuncionario(Funcionario funcionario) {
+    
+    //metodo de atualizar funcionario baseado no cliente !!!
+    public boolean atualizarFuncioanrio(Funcionario funcionario) {
         try {
             EntityManager em = emf.createEntityManager();
             em.getTransaction().begin();
@@ -42,16 +59,17 @@ public class FuncionarioRepository {
             em.close();
             return true;
         } catch (Exception e) {
-            System.out.println("Erro ao atualizar o funcionário.");
+            System.out.println("Erro ao atualizar o funcionario.");
             return false;
         }
     }
 
-    public boolean deletarFuncionario(Long id) {
+    //metodo de deletar funcionario baseado no cliente !!!
+    public boolean deletarFuncionario(String cpf) {
         try {
             EntityManager em = emf.createEntityManager();
             em.getTransaction().begin();
-            Funcionario funcionario = em.find(Funcionario.class, id);
+            Funcionario funcionario = em.find(Funcionario.class, cpf);
             em.remove(funcionario);
             em.getTransaction().commit();
             em.close();
@@ -61,10 +79,14 @@ public class FuncionarioRepository {
             return false;
         }
     }
+
+    //metodo de autenticar funcionario cod by ChatGPT (ass.Thailan)
     public Funcionario autenticarFuncionario(String username, String password) {
         try {
             EntityManager em = emf.createEntityManager();
-            return em.createQuery("SELECT f FROM Funcionario f WHERE f.username = :username AND f.password = :password", Funcionario.class)
+            return em
+                    .createQuery("SELECT f FROM Funcionario f WHERE f.username = :username AND f.password = :password",
+                            Funcionario.class)
                     .setParameter("username", username)
                     .setParameter("password", password)
                     .getSingleResult();
